455f15a10fcbd327580e81a26b630a34e89cfd96
eliyevu <kancartos6@gmail.com>
2024-12-19 11:24:18 +0000

    'complete'

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,10 +4,7 @@
 package org.example;
 
 public class App {
-  public String getGreeting() {
-        return "Welcome to to the library!." ;
-    }
-
+ 
     public static void main(String[] args) {
 
         System.out.println(new App().getGreeting());
